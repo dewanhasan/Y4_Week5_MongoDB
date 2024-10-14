0744baf00bb32ae8fc57af1f62ea54d32d9aba43
dewanhasan <g00410156@atu.ie>
2024-10-14 17:05:39 +0100

    Added Validations

--- src/main/java/ie/atu/week5/customerapp/ControllerAdvice.java
+++ src/main/java/ie/atu/week5/customerapp/ControllerAdvice.java
@@ -1,4 +1,18 @@
 package ie.atu.week5.customerapp;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+
 public class ControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleValidationExceptions(MethodArgumentNotValidException ex) {
+        return "Error Occured" + ex.getBindingResult().getFieldError().getDefaultMessage();
+    }
 }
